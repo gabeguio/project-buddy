1e96b289105a59c0d4e300af9e62b1cfdf7ea8b5
gguio <gdguio@outlook.com>
2023-04-11 11:39:16 -0400

    Ticket Pojo, TicketDao, TicketModel

---
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dynamodb/TicketDao.java
@@ -0,0 +1,23 @@
+package com.nashss.se.musicplaylistservice.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.nashss.se.musicplaylistservice.dynamodb.models.Ticket;
+
+public class TicketDao {
+    private final DynamoDBMapper dynamoDBMapper;
+
+    public TicketDao(DynamoDBMapper dynamoDBMapper) {
+        this.dynamoDBMapper = dynamoDBMapper;
+    }
+
+    public Ticket getTicket(String projectId, String ticketId) {
+        Ticket ticket = this.dynamoDBMapper.load(Ticket.class, projectId, ticketId);
+
+        return ticket;
+    }
+
+    public Ticket saveTicket(Ticket ticket) {
+        this.dynamoDBMapper.save(ticket);
+        return ticket;
+    }
+}
