c2aea4031d2b5e0e74640adbf5fd3f75f0b525dd
Tyler <mm1231580@gmail.com>
2023-04-11 11:37:14 -0500

    implemented project Table in yaml, setup corresponding POJO and model classes and implemented basic DAO

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dynamodb/ProjectDao.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dynamodb/ProjectDao.java
@@ -1,5 +1,6 @@
 package com.nashss.se.musicplaylistservice.dynamodb;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -10,7 +11,8 @@ import com.nashss.se.musicplaylistservice.metrics.MetricsPublisher;
 public class ProjectDao {
     private final DynamoDBMapper dynamoDBMapper;
 
-    public ProjectDao(DynamoDBMapper mapper, MetricsPublisher publisher) {
+    @Inject
+    public ProjectDao(DynamoDBMapper mapper) {
         this.dynamoDBMapper = mapper;
     }
 
