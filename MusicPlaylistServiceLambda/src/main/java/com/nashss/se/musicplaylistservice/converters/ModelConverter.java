30a187a96ff58b15767ddcc714aef2225799d423
Tyler <mm1231580@gmail.com>
2023-04-12 09:31:01 -0500

    moved changed model converter to new class temporarily to avoid build errors.

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/converters/ModelConverter.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/converters/ModelConverter.java
@@ -1,9 +1,9 @@
 package com.nashss.se.musicplaylistservice.converters;
 
-import com.nashss.se.musicplaylistservice.dynamodb.models.Project;
-import com.nashss.se.musicplaylistservice.dynamodb.models.Ticket;
-import com.nashss.se.musicplaylistservice.models.ProjectModel;
-import com.nashss.se.musicplaylistservice.models.TicketModel;
+import com.nashss.se.musicplaylistservice.dynamodb.models.AlbumTrack;
+import com.nashss.se.musicplaylistservice.dynamodb.models.Playlist;
+import com.nashss.se.musicplaylistservice.models.PlaylistModel;
+import com.nashss.se.musicplaylistservice.models.SongModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,36 +13,71 @@ import java.util.List;
  */
 public class ModelConverter {
     /**
-     * Converts a provided {@link Project} into a {@link ProjectModel} representation.
+     * Converts a provided {@link Playlist} into a {@link PlaylistModel} representation.
      *
-     * @param project the project to convert
-     * @return the converted project
+     * @param playlist the playlist to convert
+     * @return the converted playlist
      */
-    public ProjectModel toProjectModel(Project project) {
+    public PlaylistModel toPlaylistModel(Playlist playlist) {
+        List<String> tags = null;
+        if (playlist.getTags() != null) {
+            tags = new ArrayList<>(playlist.getTags());
+        }
 
-        return ProjectModel.builder()
-                .withProjectId(project.getProjectId())
-                .withTitle(project.getTitle())
-                .withDescription(project.getDescription())
-                .withStatus(project.getStatus())
-                .build();
+        return PlaylistModel.builder()
+            .withId(playlist.getId())
+            .withName(playlist.getName())
+            .withCustomerId(playlist.getCustomerId())
+            .withCustomerName(playlist.getCustomerName())
+            .withSongCount(playlist.getSongCount())
+            .withTags(tags)
+            .build();
     }
 
     /**
-     * Converts a provided Ticket into a TicketModel representation.
+     * Converts a provided AlbumTrack into a SongModel representation.
      *
-     * @param ticket the ticket to convert to TicketModel
+     * @param albumTrack the AlbumTrack to convert to SongModel
      * @return the converted SongModel with fields mapped from albumTrack
      */
-    public TicketModel toTicketModel(Ticket ticket) {
-        return TicketModel.builder()
-             .withProjectId(ticket.getProjectId())
-             .withTicketId(ticket.getTicketId())
-             .withTitle(ticket.getTitle())
-             .withDescription(ticket.getDescription())
-             .withStatus(ticket.getStatus())
-             .build();
+    public SongModel toSongModel(AlbumTrack albumTrack) {
+        return SongModel.builder()
+            .withAsin(albumTrack.getAsin())
+            .withTrackNumber(albumTrack.getTrackNumber())
+            .withAlbum(albumTrack.getAlbumName())
+            .withTitle(albumTrack.getSongTitle())
+            .build();
     }
 
+    /**
+     * Converts a list of AlbumTracks to a list of SongModels.
+     *
+     * @param albumTracks The AlbumTracks to convert to SongModels
+     * @return The converted list of SongModels
+     */
+    public List<SongModel> toSongModelList(List<AlbumTrack> albumTracks) {
+        List<SongModel> songModels = new ArrayList<>();
 
+        for (AlbumTrack albumTrack : albumTracks) {
+            songModels.add(toSongModel(albumTrack));
+        }
+
+        return songModels;
+    }
+
+    /**
+     * Converts a list of Playlists to a list of PlaylistModels.
+     *
+     * @param playlists The Playlists to convert to PlaylistModels
+     * @return The converted list of PlaylistModels
+     */
+    public List<PlaylistModel> toPlaylistModelList(List<Playlist> playlists) {
+        List<PlaylistModel> playlistModels = new ArrayList<>();
+
+        for (Playlist playlist : playlists) {
+            playlistModels.add(toPlaylistModel(playlist));
+        }
+
+        return playlistModels;
+    }
 }
---
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/converters/ProjectModelConverter.java
@@ -0,0 +1,47 @@
+package com.nashss.se.musicplaylistservice.converters;
+
+import com.nashss.se.musicplaylistservice.dynamodb.models.Project;
+import com.nashss.se.musicplaylistservice.dynamodb.models.Ticket;
+import com.nashss.se.musicplaylistservice.models.ProjectModel;
+import com.nashss.se.musicplaylistservice.models.TicketModel;
+
+public class ProjectModelConverter{
+    /**
+     * Converts between Data and API models.
+     */
+        /**
+         * Converts a provided {@link Project} into a {@link ProjectModel} representation.
+         *
+         * @param project the project to convert
+         * @return the converted project
+         */
+        public ProjectModel toProjectModel(Project project) {
+
+            return ProjectModel.builder()
+                .withProjectId(project.getProjectId())
+                .withTitle(project.getTitle())
+                .withDescription(project.getDescription())
+                .withStatus(project.getStatus())
+                .build();
+        }
+
+        /**
+         * Converts a provided Ticket into a TicketModel representation.
+         *
+         * @param ticket the ticket to convert to TicketModel
+         * @return the converted SongModel with fields mapped from albumTrack
+         */
+        public TicketModel toTicketModel(Ticket ticket) {
+            return TicketModel.builder()
+                .withProjectId(ticket.getProjectId())
+                .withTicketId(ticket.getTicketId())
+                .withTitle(ticket.getTitle())
+                .withDescription(ticket.getDescription())
+                .withStatus(ticket.getStatus())
+                .build();
+        }
+
+
+    }
+
+
