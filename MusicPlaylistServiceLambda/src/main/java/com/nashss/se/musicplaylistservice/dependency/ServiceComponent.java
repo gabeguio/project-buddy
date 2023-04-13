15c3ecc097557fb8a47127a78d1f185c0f62a38d
gguio <gdguio@outlook.com>
2023-04-13 15:02:36 -0400

    Added CreateTicketLambda

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dependency/ServiceComponent.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dependency/ServiceComponent.java
@@ -1,12 +1,9 @@
 package com.nashss.se.musicplaylistservice.dependency;
 
-import com.nashss.se.musicplaylistservice.activity.AddSongToPlaylistActivity;
-import com.nashss.se.musicplaylistservice.activity.CreatePlaylistActivity;
-import com.nashss.se.musicplaylistservice.activity.GetPlaylistActivity;
-import com.nashss.se.musicplaylistservice.activity.GetPlaylistSongsActivity;
-import com.nashss.se.musicplaylistservice.activity.SearchPlaylistsActivity;
-import com.nashss.se.musicplaylistservice.activity.UpdatePlaylistActivity;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.musicplaylistservice.activity.*;
 
+import com.nashss.se.musicplaylistservice.activity.requests.CreateTicketRequest;
 import dagger.Component;
 
 import javax.inject.Singleton;
@@ -54,4 +51,5 @@ public interface ServiceComponent {
      */
     UpdatePlaylistActivity provideUpdatePlaylistActivity();
 
+    CreateTicketActivity provideCreateTicketActivity();
 }
