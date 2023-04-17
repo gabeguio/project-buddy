472980cd8ae25d27c6bd1117c4b503f6f463a762
Terrence Atkinson Jr <tatkinsonjr2@hotmail.com>
2023-04-17 12:24:52 -0400

    Test implemented for the handlRequest and build succeeds

--- MusicPlaylistServiceLambda/src/test/java/com/nashss/se/musicplaylistservice/activity/DeleteTicketActivityTest.java
+++ MusicPlaylistServiceLambda/src/test/java/com/nashss/se/musicplaylistservice/activity/DeleteTicketActivityTest.java
@@ -1,12 +1,16 @@
 package com.nashss.se.musicplaylistservice.activity;
 
+import com.nashss.se.musicplaylistservice.activity.requests.DeleteTicketRequest;
+import com.nashss.se.musicplaylistservice.activity.results.DeleteTicketResult;
 import com.nashss.se.musicplaylistservice.dynamodb.ProjectDao;
 import com.nashss.se.musicplaylistservice.dynamodb.TicketDao;
 import com.nashss.se.musicplaylistservice.dynamodb.models.Ticket;
+import com.nashss.se.musicplaylistservice.models.TicketModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -25,16 +29,33 @@ public class DeleteTicketActivityTest {
 
     @Test
     public void handleRequest_withTicketIdAndProjectId_deletesTicket(){
-        //GIVEN
-        String projectId = "expectedId";
-        String ticketId = "expectedTicketId";
-        Ticket ticketToBeDeleted = new Ticket();
-        when(ticketDao.getTicket(projectId,ticketId)).thenReturn(ticketToBeDeleted);
+        // Given
+        DeleteTicketRequest request = DeleteTicketRequest.builder()
+                .withTicketId("123")
+                .withProjectId("456")
+                .withTitle("Ticket Title")
+                .withDescription("Ticket Description")
+                .withStatus("open")
+                .build();
 
-        //WHEN
-        deleteTicketActivity.handleRequest(ticketId,projectId);
+        Ticket ticket = new Ticket();
+        ticket.setTicketId(request.getTicketId());
+        ticket.setProjectId(request.getProjectId());
+        ticket.setDescription(request.getDescription());
+        ticket.setStatus(request.getStatus());
+        ticket.setTitle(request.getTitle());
 
-        //THEN
-        verify(ticketDao).deleteTicket(ticketToBeDeleted);
+        // When
+        DeleteTicketResult result = deleteTicketActivity.handleRequest(request);
+
+        // Then
+        verify(ticketDao).deleteTicket(ticket);
+
+        TicketModel ticketModel = result.getTicket();
+        assertEquals(request.getTicketId(), ticketModel.getTicketId());
+        assertEquals(request.getProjectId(), ticketModel.getProjectId());
+        assertEquals(request.getTitle(), ticketModel.getTitle());
+        assertEquals(request.getDescription(), ticketModel.getDescription());
+        assertEquals(request.getStatus(), ticketModel.getStatus());
     }
 }
