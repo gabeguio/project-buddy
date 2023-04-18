d720b66114af1b26a846f09ccba1be06429a1e7f
Terrence Atkinson Jr <tatkinsonjr2@hotmail.com>
2023-04-18 10:27:57 -0400

    Request completed

---
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/UpdateProjectStatusRequest.java
@@ -0,0 +1,52 @@
+package com.nashss.se.musicplaylistservice.activity.requests;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.apache.commons.lang3.builder.Builder;
+
+public class UpdateProjectStatusRequest {
+
+    private final String status;
+    private final String projectId;
+
+    private UpdateProjectStatusRequest(String projectId, String status){
+        this.status = status;
+        this.projectId = projectId;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public String getProjectId(){
+        return projectId;
+    }
+
+    @Override
+    public String toString(){
+    return "UpdateProjectStatusRequest{" + "projectId='" + projectId + '\'' + ", status='" + status + '}';
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String status;
+        private String projectId;
+
+        public Builder withStatus(String status){
+            this.status = status;
+            return this;
+        }
+
+        public Builder withProjectId(){
+            this.projectId = projectId;
+            return this;
+        }
+
+        public UpdateProjectStatusRequest build(){
+            return new UpdateProjectStatusRequest(projectId, status);
+        }
+    }
+}
