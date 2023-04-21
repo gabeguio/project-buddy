e5d4c2f3d03f9b53bb36ff0b64040c8f9b663f3e
Daniel Mikos <mikosd@live.com>
2023-04-21 15:45:51 -0500

    create ticket works, the redirect page is not redirecting to new tickets

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
   },
   entry: {
     createProject: path.resolve(__dirname, 'src', 'pages', 'createProject.js'),
+    createTicket: path.resolve(__dirname, 'src', 'pages', 'createTicket.js'),
     loadProjects: path.resolve(__dirname, 'src', 'pages', 'loadProjects.js'),
     viewPlaylist: path.resolve(__dirname, 'src', 'pages', 'viewPlaylist.js'),
     viewProject: path.resolve(__dirname, 'src', 'pages', 'viewProject.js'),
