fd405eb1eac09401a46711f24e06defe926ba163
gguio <gdguio@outlook.com>
2023-12-11 14:46:58 -0500

    Added Mobile Menu Navigation

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,5 +1,6 @@
 import ProjectClient from "../api/ProjectClient";
 import BindingClass from "../util/BindingClass";
+import mobileNav from "../util/mobile-nav";
 
 /**
  * The header component for the website.
@@ -41,7 +42,12 @@ export default class Header extends BindingClass {
         <li>
           <button class="btn header__link" id="header__user"></button>
         </li>
-      <ul>
+      </ul>
+      <button class="header__bars">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd"/>
+      </svg>
+    </button>
     </nav>
   `;
 
@@ -52,6 +58,8 @@ export default class Header extends BindingClass {
     // Retrieve user information, if not user is logged in, then return 'Login'
     const currentUser = await this.client.getIdentity();
     this.createUserInfoForHeader(currentUser);
+
+    mobileNav();
   }
 
   // If current user is not undefined, then create logout button by retrieving the name of the current user.
