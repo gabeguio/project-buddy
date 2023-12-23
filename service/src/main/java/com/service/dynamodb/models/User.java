d7902695c4fc5093e1f9e95b5996590a6f6d1ced
gguio <gdguio@outlook.com>
2023-12-23 17:55:04 -0500

    User DynamoDb Class

--- service/src/main/java/com/service/dynamodb/models/User.java
+++ service/src/main/java/com/service/dynamodb/models/User.java
@@ -1,10 +1,96 @@
 package com.service.dynamodb.models;
 
-public class User {
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "users")
+public class User {
+    private String userId;
+    private String dateCreated;
+    private String dateLastUpdated;
     private String firstName;
     private String lastName;
-    private String role;
     private String company;
     private String email;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "dateCreated")
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @DynamoDBAttribute(attributeName = "dateLastUpdated")
+    public String getDateLastUpdated() {
+        return dateLastUpdated;
+    }
+
+    public void setDateLastUpdated(String dateLastUpdated) {
+        this.dateLastUpdated = dateLastUpdated;
+    }
+
+    @DynamoDBAttribute(attributeName = "firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @DynamoDBAttribute(attributeName = "lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @DynamoDBAttribute(attributeName = "company")
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    @DynamoDBAttribute(attributeName = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId) && Objects.equals(dateCreated, user.dateCreated) && Objects.equals(dateLastUpdated, user.dateLastUpdated) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(company, user.company) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, dateCreated, dateLastUpdated, firstName, lastName, company, email);
+    }
 }
+
